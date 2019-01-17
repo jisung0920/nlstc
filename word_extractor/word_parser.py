010da16038a3e4101e6958e23080d077801bf7aa
정지성 <jisung0920@gmail.com>
2019-01-18 00:39:30 +0900

    word parser

--- word_extractor/word_parser.py
+++ word_extractor/word_parser.py
@@ -3,43 +3,33 @@ import json
 from rake_nltk import Rake
 from vocabulary.vocabulary import Vocabulary as vb
 
-
-def query_generator(sentence,key_num,syn_num,file_types) :
-
-    parser = WordParser(sentence,key_num,syn_num)
-    keywords = parser.get_keywords()
-    key_tokens = parser.get_tokens(keywords)
-
-    return parser.get_query(key_tokens,file_types)
-
-def searching_word_generator(sentence,key_num,syn_num) :
-
-    parser = WordParser(sentence,key_num,syn_num)
-    keywords = parser.get_keywords()
-    key_tokens = parser.get_tokens(keywords)
-    syn_words = parser.get_keyword_synonym(key_tokens)
-    search_word = key_tokens + syn_words
-
-    return search_word
-
 class WordParser:
+
     sentence = ""
+    keywords = []
+    key_tokens = []
+    syn_words = []
     keyword_limit = 3
     synonym_limit = 2
 
     def __init__(self, sentence, keyword_limit=3, synonym_limit=2):
+
         self.sentence = sentence
         self.keyword_limit = keyword_limit
         self.synonym_limit = synonym_limit
 
-    def get_keywords(self):
+        self.keywords = self.parse_keywords()
+        self.key_tokens = self.parse_tokens(self.keywords)
+        self.syn_words = self.parse_keyword_synonym(self.key_tokens)
+
+    def parse_keywords(self):
         sentences = [self.sentence]
         r = Rake()
         r.extract_keywords_from_sentences(sentences)
         keywords = r.ranked_phrases
         return keywords[0:self.keyword_limit]
 
-    def get_tokens(self,keywords):
+    def parse_tokens(self,keywords):
         tokens = []
         for keyword in keywords :
             raw_tokens = keyword.split(' ')
@@ -49,7 +39,7 @@ class WordParser:
                 tokens.append(token)
         return tokens
 
-    def get_keyword_synonym(self, tokens):
+    def parse_keyword_synonym(self, tokens):
         synonyms = []
         # print vb.synonym("test")
         for token in tokens:
@@ -66,10 +56,12 @@ class WordParser:
                 continue
         return synonyms
 
-    @staticmethod
-    def get_query(words, file_types):
+    def query_generator(self, file_types):
         querys = []
-        words_txt = '~' + " or ~".join(words)
+        words_txt = '~' + " or ~".join(self.key_tokens)
         for file_type in file_types :
             querys.append(words_txt + " filetype:" + file_type)
         return querys
+
+    def searching_word_generator(self):
+        return self.key_tokens + self.syn_words
